33584ea4098c18daeca2c6f2e7bf43f8d1e53e2c
Sean McGivern <sean@mcgivern.me.uk>
2015-08-06 11:05:27 +0100

    `lein new app`

--- src/gitique_examples/core.clj
+++ src/gitique_examples/core.clj
@@ -1,6 +1,7 @@
-(ns gitique-examples.core)
+(ns gitique-examples.core
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
